37086210c8901f5816e51966d1ca190799168163
macazava <macazava@gmail.com>
2025-11-08 17:57:36 +0000

    instalei duas implementations para conectar com o springboot

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -58,6 +58,9 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- frontend/app/src/main/java/pt/iade/ei/waycareapp/ui/screens/reporte/ReportScreen.kt
+++ frontend/app/src/main/java/pt/iade/ei/waycareapp/ui/screens/reporte/ReportScreen.kt
@@ -1,11 +1,9 @@
 package pt.iade.ei.waycareapp.ui.screens.reporte
 
-import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -21,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
